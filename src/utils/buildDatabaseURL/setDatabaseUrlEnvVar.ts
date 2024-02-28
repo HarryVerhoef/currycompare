b43f235cf067eac259b02b0b39a5f56d11cab3c1
HarryVerhoef <hverhoef@guidr.legal>
2024-02-28 23:04:35 +0000

    testing

--- src/utils/buildDatabaseURL/setDatabaseUrlEnvVar.ts
+++ src/utils/buildDatabaseURL/setDatabaseUrlEnvVar.ts
@@ -2,6 +2,7 @@ import { buildDatabaseURL } from ".";
 
 try {
   process.env.DATABASE_URL = buildDatabaseURL();
+  console.log(buildDatabaseURL());
 } catch {
   console.error("Something went wrong building the database URL");
 }
