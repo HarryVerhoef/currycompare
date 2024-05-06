2df05505b3c6a22f23cf18bc238acfeac6a70c9c
HarryVerhoef <hverhoef@guidr.legal>
2024-05-06 14:54:14 +0100

    Use branch name as ref

--- src/ci/scripts/triggerLambdaDeployments.ts
+++ src/ci/scripts/triggerLambdaDeployments.ts
@@ -2,4 +2,5 @@ import triggerLambdaDeployments from "../triggerLambdaDeployments";
 
 triggerLambdaDeployments().catch((error) => {
   console.error(error);
+  throw error;
 });
--- src/ci/triggerLambdaDeployments/index.ts
+++ src/ci/triggerLambdaDeployments/index.ts
@@ -23,6 +23,17 @@ const triggerLambdaDeployments = async (): Promise<void> => {
       ref: sha,
     });
 
+    const pushedToBranches = await rest.repos.listBranchesForHeadCommit({
+      owner: REPO_OWNER,
+      repo: REPO_NAME,
+      commit_sha: sha,
+    });
+
+    console.log(`Branches where commit ${sha} is head:`);
+    console.log(JSON.stringify(pushedToBranches));
+
+    const ref = pushedToBranches.data[0].name;
+
     const changedLambdas =
       response.data.files
         ?.map(({ filename: filepath }) => getLambdaKeyFromPath(filepath))
@@ -37,7 +48,7 @@ const triggerLambdaDeployments = async (): Promise<void> => {
           owner: REPO_OWNER,
           repo: REPO_NAME,
           workflow_id: "deploy-lambda.yml",
-          ref: sha,
+          ref,
           inputs: {
             lambdaKey: key,
           },
