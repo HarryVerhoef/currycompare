4a685dcce2a98c90cb743d5be0432b172f01baaf
HarryVerhoef <hverhoef@guidr.legal>
2024-05-20 20:52:34 +0100

    Tests for GET /curryhouses bad request

--- src/lambdas/curryhouses/get/index.test.ts
+++ src/lambdas/curryhouses/get/index.test.ts
@@ -19,14 +19,102 @@ describe("GET /curryhouses", () => {
     expect(console.log).toHaveBeenCalledWith("Request event:", dummyEvent);
   });
 
-  it("should return hello world with a 200 status code", async () => {
-    const { statusCode, body } = await handler(
-      dummyEvent,
-      dummyContext,
-      () => {},
-    );
+  it("should respond with a 400 if lng is not included as a query string parameter", async () => {
+    const event = buildLambdaEvent({
+      queryStringParameters: {
+        lat: "10",
+        rad: "5",
+      },
+    });
 
-    expect(statusCode).toBe(200);
-    expect(body).toBe("hello, world");
+    const { statusCode, body } = await handler(event, dummyContext, () => {});
+
+    expect(statusCode).toBe(400);
+    expect(JSON.parse(body ?? "{}").errorMsg).toBe(
+      "Missing required query string parameter: 'lng'",
+    );
+  });
+
+  it("should respond with a 400 if lat is not included as a query string parameter", async () => {
+    const event = buildLambdaEvent({
+      queryStringParameters: {
+        lng: "10",
+        rad: "5",
+      },
+    });
+
+    const { statusCode, body } = await handler(event, dummyContext, () => {});
+
+    expect(statusCode).toBe(400);
+    expect(JSON.parse(body ?? "{}").errorMsg).toBe(
+      "Missing required query string parameter: 'lat'",
+    );
+  });
+
+  it("should respond with a 400 if rad is not included as a query string parameter", async () => {
+    const event = buildLambdaEvent({
+      queryStringParameters: {
+        lat: "10",
+        lng: "10",
+      },
+    });
+
+    const { statusCode, body } = await handler(event, dummyContext, () => {});
+
+    expect(statusCode).toBe(400);
+    expect(JSON.parse(body ?? "{}").errorMsg).toBe(
+      "Missing required query string parameter: 'rad'",
+    );
+  });
+
+  it("should respond with a 400 if lat is not a number", async () => {
+    const event = buildLambdaEvent({
+      queryStringParameters: {
+        lat: "ten",
+        lng: "10",
+        rad: "5",
+      },
+    });
+
+    const { statusCode, body } = await handler(event, dummyContext, () => {});
+
+    expect(statusCode).toBe(400);
+    expect(JSON.parse(body ?? "{}").errorMsg).toBe(
+      "Expected 'lat' to be a number",
+    );
+  });
+
+  it("should respond with a 400 if lng is not a number", async () => {
+    const event = buildLambdaEvent({
+      queryStringParameters: {
+        lat: "10",
+        lng: "ten",
+        rad: "5",
+      },
+    });
+
+    const { statusCode, body } = await handler(event, dummyContext, () => {});
+
+    expect(statusCode).toBe(400);
+    expect(JSON.parse(body ?? "{}").errorMsg).toBe(
+      "Expected 'lat' to be a number",
+    );
+  });
+
+  it("should respond with a 400 if rad is not a number", async () => {
+    const event = buildLambdaEvent({
+      queryStringParameters: {
+        lat: "10",
+        lng: "10",
+        rad: "five",
+      },
+    });
+
+    const { statusCode, body } = await handler(event, dummyContext, () => {});
+
+    expect(statusCode).toBe(400);
+    expect(JSON.parse(body ?? "{}").errorMsg).toBe(
+      "Expected 'rad' to be a number",
+    );
   });
 });
