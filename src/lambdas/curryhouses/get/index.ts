cd6079933671b295003e57b97cf28ff456dc0f41
HarryVerhoef <hverhoef@guidr.legal>
2024-06-30 16:18:15 +0100

    Force lambda deployment

--- src/lambdas/curryhouses/get/index.ts
+++ src/lambdas/curryhouses/get/index.ts
@@ -31,8 +31,8 @@ export const handler: LambdaHandler = async (event) => {
 
   const prisma = new PrismaClient({ datasourceUrl: dbUrl });
 
-  // TODO: Need to ensure this is safe from injection.
-  // We are using a raw query here because prisma does not support postgis (geospatial queries).
+  // TODO: Need to ensure this is safe from injection
+  // We are using a raw query here because prisma does not support postgis (geospatial queries)
   const curryhouses = await prisma.$queryRaw<Curryhouse[]>`
     SELECT *
     FROM Curryhouses
