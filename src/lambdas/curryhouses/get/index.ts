dc909389a011067ca8ceb862721e61efab499f2d
HarryVerhoef <hverhoef@guidr.legal>
2024-07-21 16:08:09 +0100

    Add buildLambdaHandler wrapper

--- src/lambdas/curryhouses/get/index.ts
+++ src/lambdas/curryhouses/get/index.ts
@@ -1,38 +1,32 @@
-import { isLeft } from "fp-ts/lib/Either";
 import { type LambdaHandler } from "../../../types/lambda";
-import { parseGetCurryhouseEvent } from "./parser";
-import buildLambdaError, { StatusCode } from "../../../utils/buildLambdaError";
 import { PrismaClient } from "../../../prisma/generated";
 import convertSearchRadiusToMetres from "../../../utils/convertSearchRadiusToMetres";
 import { buildDatabaseURL } from "../../../utils/buildDatabaseURL";
 import { type CurryhouseLatLng } from "../../../codecs/CurryhouseLatLng";
+import buildLambdaHandler from "../../../utils/buildLambdaHandler";
+import { getCurryhousesQueryStringParams } from "../../../types/api/curryhouses/get";
 
 // https://www.prisma.io/docs/orm/prisma-client/deployment/serverless/deploy-to-aws-lambda#deploy-only-the-required-files
 
-export const getCurryhousesName = "GetCurryhouses";
+export const handler: LambdaHandler = buildLambdaHandler({
+  queryCodec: getCurryhousesQueryStringParams,
+  handler: async (event) => {
+    console.log("Request event:", event);
 
-export const handler: LambdaHandler = async (event) => {
-  console.log("Request event:", event);
+    const { lat, lng, rad } = event.queryStringParameters;
 
-  const parseResult = parseGetCurryhouseEvent(event);
+    console.log(
+      `Finding all curryhouses with latitude=${lat}, longitude=${lng}, radius=${rad}...`,
+    );
 
-  if (isLeft(parseResult))
-    return buildLambdaError(StatusCode.BAD_REQUEST, parseResult.left);
+    const dbUrl = await buildDatabaseURL();
 
-  const { lat, lng, rad } = parseResult.right.queryStringParameters;
+    console.log(`Database URL: ${dbUrl}`);
 
-  console.log(
-    `Finding all curryhouses with latitude=${lat}, longitude=${lng}, radius=${rad}...`,
-  );
+    const prisma = new PrismaClient({ datasourceUrl: dbUrl });
 
-  const dbUrl = await buildDatabaseURL();
-
-  console.log(`Database URL: ${dbUrl}`);
-
-  const prisma = new PrismaClient({ datasourceUrl: dbUrl });
-
-  // We are using a raw query here because prisma does not support postgis (geospatial queries)
-  const curryhouses = await prisma.$queryRaw<CurryhouseLatLng[]>`
+    // We are using a raw query here because prisma does not support postgis (geospatial queries)
+    const curryhouses = await prisma.$queryRaw<CurryhouseLatLng[]>`
     SELECT
       "id",
       "title",
@@ -47,20 +41,21 @@ export const handler: LambdaHandler = async (event) => {
       ${convertSearchRadiusToMetres(rad)}
     );`;
 
-  if (curryhouses.length > 0) {
-    console.log(`Found ${curryhouses.length} curryhouses:`);
-    console.log(curryhouses.map((curryhouse) => curryhouse.title).join(", "));
-  } else {
-    console.log("Found no curryhouses within the search area.");
-  }
+    if (curryhouses.length > 0) {
+      console.log(`Found ${curryhouses.length} curryhouses:`);
+      console.log(curryhouses.map((curryhouse) => curryhouse.title).join(", "));
+    } else {
+      console.log("Found no curryhouses within the search area.");
+    }
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      curryhouses,
-    }),
-  };
-};
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        curryhouses,
+      }),
+    };
+  },
+});
