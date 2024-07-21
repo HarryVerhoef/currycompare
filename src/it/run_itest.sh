77babc6eab6baa8acb6a9ccf67d01862cc5312ab
HarryVerhoef <hverhoef@guidr.legal>
2024-07-21 15:23:12 +0100

    Run integration tests in container

--- src/it/run_itest.sh
+++ src/it/run_itest.sh
@@ -2,5 +2,4 @@ npm install
 npm run serve:local:clean:background
 sleep 20 # Temporary fix to wait for database to be healthy - postgis extension means typical healthcheck is more complicated
 npm run db:reset:local
-CURRYCOMPARE_ENVIRONMENT=local npm run test:integration
-exit $?
+npm run test:integration
