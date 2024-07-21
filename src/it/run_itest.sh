4cf32198897265fa527a6111fa854ee71d39c86c
HarryVerhoef <hverhoef@guidr.legal>
2024-07-21 14:58:37 +0100

    Add exit code to integration tests

--- src/it/run_itest.sh
+++ src/it/run_itest.sh
@@ -3,4 +3,4 @@ npm run serve:local:clean:background
 sleep 20 # Temporary fix to wait for database to be healthy - postgis extension means typical healthcheck is more complicated
 npm run db:reset:local
 CURRYCOMPARE_ENVIRONMENT=local npm run test:integration
-
+exit $?
