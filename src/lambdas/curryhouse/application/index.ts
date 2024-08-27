f4bae588d581c53b7839b799fa44671947e1f4e2
HarryVerhoef <hverhoef@guidr.legal>
2024-08-27 19:11:13 +0100

    Force jobs

--- src/lambdas/curryhouse/application/index.ts
+++ src/lambdas/curryhouse/application/index.ts
@@ -1,6 +1,8 @@
 import { type LambdaHandler } from "../../../types/lambda";
 import buildLambdaHandler from "../../../utils/buildLambdaHandler";
 
+// force job
+
 export const PostCurryhouseApplicationName = "SubmitCurryhouseApplication";
 
 export const handler: LambdaHandler = buildLambdaHandler({
