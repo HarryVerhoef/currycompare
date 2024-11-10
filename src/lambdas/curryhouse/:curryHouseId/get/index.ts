2ed089cfa037a090fe22c5d3321d58b0ef73d09d
HarryVerhoef <hverhoef@guidr.legal>
2024-11-10 15:52:53 +0000

    Add to lambdaName

--- src/lambdas/curryhouse/:curryHouseId/get/index.ts
+++ src/lambdas/curryhouse/:curryHouseId/get/index.ts
@@ -6,7 +6,7 @@ import { type CurryhouseLatLng } from "../../../../codecs/CurryhouseLatLng";
 
 export const getCurryhouseName = "GetCurryhouse";
 
-// force job
+// force job 2
 export const handler = buildLambdaHandler({
   pathCodec: getCurryhousePathParams,
   handler: async (event) => {
